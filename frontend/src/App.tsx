b751a1c1420966bd9e57e07651b2b6ff5617bc8c
Khaym03 <daniellarosa20003@gmail.com>
2025-07-25 11:33:09 -0400

    added Cosmic Night theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import logo from './assets/images/logo-universal.png'
-import './App.css'
 import { Greet } from '../wailsjs/go/main/App'
 import { Button } from '@/components/ui/button'
+import { ThemeProvider } from './components/theme-provider'
 
 function App() {
   return (
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
     </div>
+    </ThemeProvider>
   )
 }
 
